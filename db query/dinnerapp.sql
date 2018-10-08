38eaef3c197c43f413e3b1d14bcf9485abf5da29
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-10-08 16:12:21 +0300

    Add create dinnerapp query

---
+++ db query/dinnerapp.sql
@@ -0,0 +1,155 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dinnerapp
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(70) NOT NULL,
+  `category_price` int(11) NOT NULL,
+  PRIMARY KEY (`id_category`),
+  UNIQUE KEY `id_categorie_UNIQUE` (`id_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food_category`
+--
+
+DROP TABLE IF EXISTS `food_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `food_category` (
+  `id_food_type` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  PRIMARY KEY (`id_food_type`,`id_category`),
+  KEY `id_category_fk_idx` (`id_category`),
+  CONSTRAINT `id_category_fk` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `id_food_type_fk` FOREIGN KEY (`id_food_type`) REFERENCES `food_type` (`id_food_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_category`
+--
+
+LOCK TABLES `food_category` WRITE;
+/*!40000 ALTER TABLE `food_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food_type`
+--
+
+DROP TABLE IF EXISTS `food_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `food_type` (
+  `id_food_type` int(11) NOT NULL AUTO_INCREMENT,
+  `food_name` varchar(70) NOT NULL,
+  `food_ingredients` varchar(100) DEFAULT NULL,
+  `food_weight` int(11) DEFAULT NULL,
+  `food_price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_food_type`),
+  UNIQUE KEY `idfood_tyoe_UNIQUE` (`id_food_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_type`
+--
+
+LOCK TABLES `food_type` WRITE;
+/*!40000 ALTER TABLE `food_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_date` date NOT NULL,
+  `menu_description` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_food_category`
+--
+
+DROP TABLE IF EXISTS `menu_food_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `menu_food_category` (
+  `id_menu` int(11) NOT NULL,
+  `id_food_category` int(11) NOT NULL,
+  PRIMARY KEY (`id_menu`,`id_food_category`),
+  KEY `id_food_category_fk_idx` (`id_food_category`),
+  CONSTRAINT `id_food_category_fk` FOREIGN KEY (`id_food_category`) REFERENCES `food_category` (`id_food_type`),
+  CONSTRAINT `id_menu_fk` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_food_category`
+--
+
+LOCK TABLES `menu_food_category` WRITE;
+/*!40000 ALTER TABLE `menu_food_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_food_category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-08 15:06:32
