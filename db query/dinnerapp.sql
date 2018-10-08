8802d6ed486609626bb9e69b55ba2aa44b1d7dd8
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-10-08 16:25:46 +0300

    Add new query

--- db query/dinnerapp.sql
+++ db query/dinnerapp.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `dinnerapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `dinnerapp`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dinnerapp
@@ -152,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-08 15:06:32
+-- Dump completed on 2018-10-08 16:20:23
