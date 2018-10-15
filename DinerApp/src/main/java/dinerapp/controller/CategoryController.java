2f2e30d7fb11b5d8c9b1d0d467ef316f6f004e84
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-10-15 11:59:22 +0300

    Work at CategoryController

--- DinerApp/src/main/java/dinerapp/controller/CategoryController.java
+++ DinerApp/src/main/java/dinerapp/controller/CategoryController.java
@@ -5,10 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import dinerapp.repository.CategoryRepository;
+
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
 
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping("/category")
+	public String getCategoryList(Model model) {
+		
+		
+		return "category";
+	}
+	
 	@GetMapping("/category")
 	public String getForm(Model model) {
 		return "category";
