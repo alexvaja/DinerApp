c8f383378d1c1d0a075030801b8711a493d2abf2
Silviu Sabau <sabau.silviu.bv@gmail.com>
2018-10-19 12:22:26 +0300

    Am adaugat footer, logo si navigare spre home daca apesi pe logo

--- DinerApp/src/main/java/dinerapp/controller/DinerAppController.java
+++ DinerApp/src/main/java/dinerapp/controller/DinerAppController.java
@@ -3,13 +3,9 @@ package dinerapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import dinerapp.model.FoodViewModel;
 
 @Controller
 @RequestMapping("/")
