5df3977d8e36717da9950d4b54ed7f30ace6eba3
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-11-02 13:47:33 +0200

    Refact CategoryController

--- DinerApp/src/main/java/dinerapp/controller/CategoryController.java
+++ DinerApp/src/main/java/dinerapp/controller/CategoryController.java
@@ -20,6 +20,7 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
 	@GetMapping("/categoryView")
 	public String getAllCateogires(Model model)
 	{
@@ -28,6 +29,7 @@ public class CategoryController {
 		System.out.println(getListOfCategory());
 		return "categoryView";
 	}
+	
 	@PostMapping("/categoryView")
 	public String setAllCategories(Model model, @ModelAttribute Category category, @RequestParam("submit") String reqParam)
 	{
@@ -56,45 +58,7 @@ public class CategoryController {
 		}
 		return "categoryView";
 	}
-//	@GetMapping("/allCategories")
-//	public String getAllCategories(Model model) {
-//		System.out.println("getAllCategories");
-//		
-//		CategoryViewModel categoryViewModel = new CategoryViewModel();
-//		categoryViewModel.setCategoryItems(getListOfCategory());
-//		model.addAttribute("categoryViewModel", categoryViewModel);
-//		return "allCategories";
-//	}
-//	
-//	@GetMapping("/allCategories/addCategory")
-//	public String createCategory(Model model) 
-//	{
-//    	System.out.println("createCategory");
-//    	
-//		model.addAttribute("categoryViewModel", new Category());
-//		return "editCategory";
-//	}
-//	
-//	@GetMapping("/allCategories/editCategory/{id}")
-//	public String editCategory(Model model, @PathVariable(value = "id") Integer id) {
-//		model.addAttribute("categoryViewModel", categoryRepository.findById(id));
-//		return "editCategory";
-//	}
-//	
-//	@GetMapping("/allCategories/deleteCategory/{id}")
-//	public String deleteCategory(Model model, @PathVariable(value = "id") Integer id) {
-//		categoryRepository.deleteById(id);
-//		return "redirect:/allCategories";
-//	}
-//	
-//	@PostMapping("/allCategories")
-//	public String saveCategory(Model model, @ModelAttribute Category category) {
-//    	System.out.println("saveCategory");
-//    	System.out.println(category);
-//		categoryRepository.save(category);
-//		return "redirect:/allCategories";
-//	}
-//	
+
 	private List<Category> getListOfCategory() {
 		
 		Iterable<Category> list = categoryRepository.findAll();
