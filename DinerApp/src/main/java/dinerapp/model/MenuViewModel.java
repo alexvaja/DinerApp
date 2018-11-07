64360619eaebe2f3181646f34dac14f67f0bf11c
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-11-07 16:50:19 +0200

    Add all field in MenuViewModel

--- DinerApp/src/main/java/dinerapp/model/MenuViewModel.java
+++ DinerApp/src/main/java/dinerapp/model/MenuViewModel.java
@@ -1,5 +1,59 @@
 package dinerapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dinerapp.entity.Category;
+import dinerapp.entity.Food;
+import dinerapp.entity.Menu;
+
 public class MenuViewModel {
 
+	private List<Category> categoryItems;
+	private List<Food> foodItems;
+	private List<Menu> menuItems;
+	
+	public MenuViewModel() {
+		super();
+		this.categoryItems = new ArrayList<>();
+		this.foodItems = new ArrayList<>();
+		this.menuItems = new ArrayList<>();
+	}
+	
+	public MenuViewModel(List<Category> categoryItems, List<Food> foodItems, List<Menu> menuItems) {
+		super();
+		this.categoryItems = new ArrayList<>(categoryItems);
+		this.foodItems = new ArrayList<>(foodItems);
+		this.menuItems = new ArrayList<>(menuItems);
+	}
+	
+	public List<Category> getCategoryItems() {
+		return categoryItems;
+	}
+	
+	public void setCategoryItems(List<Category> categoryItems) {
+		this.categoryItems = categoryItems;
+	}
+	
+	public List<Food> getFoodItems() {
+		return foodItems;
+	}
+	
+	public void setFoodItems(List<Food> foodItems) {
+		this.foodItems = foodItems;
+	}
+	
+	public List<Menu> getMenuItems() {
+		return menuItems;
+	}
+	
+	public void setMenuItems(List<Menu> menuItems) {
+		this.menuItems = menuItems;
+	}
+	
+	@Override
+	public String toString() {
+		return "MenuViewModel [categoryItems=" + categoryItems + ", foodItems=" + foodItems + ", menuItems=" + menuItems
+				+ "]";
+	}
 }
