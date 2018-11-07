8feea1d96a05f09a033828b00221f8183ae48f7b
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-11-07 15:57:04 +0200

    Add relation ManyToMany in Category entity

--- DinerApp/src/main/java/dinerapp/controller/MenuController.java
+++ DinerApp/src/main/java/dinerapp/controller/MenuController.java
@@ -1,13 +1,27 @@
 package dinerapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import dinerapp.entity.Category;
+import dinerapp.repository.CategoryRepository;
+import dinerapp.repository.FoodRepository;
+
 @Controller
 public class MenuController {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	@Autowired
+	FoodRepository foodRepository;
 
 	@GetMapping("/menuView")
 	public String getAllMenu(Model model) {
@@ -29,12 +43,27 @@ public class MenuController {
 			model.addAttribute("addMenuIsAvailable", addMenuIsAvailable);
 			break;
 		case "AddCategory":
+			System.out.println("Am intrat in AddCategory");
 			addMenuIsAvailable = true;
 			model.addAttribute("addMenuIsAvailable", addMenuIsAvailable);
 			addCategoryIsAvailable = true;
-			model.addAttribute("addMenuIsAvailable", addMenuIsAvailable);
+			model.addAttribute("addCategoryIsAvailable", addCategoryIsAvailable);
+			
+			List<Category> categoryList = new ArrayList<>(getListOfCategory());
+			model.addAttribute("categoryList", categoryList);
+			
 			break;
 		}
 		return "menuView";
+	}
+	
+	private List<Category> getListOfCategory() {
+		
+		Iterable<Category> list = categoryRepository.findAll();
+		List<Category> searchedList= new ArrayList<>();
+		for (Category category : list) {
+			searchedList.add(category);
+		}
+		return searchedList;
 	}
 }
--- DinerApp/src/main/java/dinerapp/entity/Category.java
+++ DinerApp/src/main/java/dinerapp/entity/Category.java
@@ -1,10 +1,17 @@
 package dinerapp.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,17 +26,24 @@ public class Category
 	private String name;
 	@Column(name="category_price")
 	private Double price;
-	
-	
+	@ManyToMany(cascade={CascadeType.ALL})
+	@JoinTable(
+			name="food_category",
+			joinColumns={@JoinColumn(name="id_category")},
+			inverseJoinColumns={@JoinColumn(name="id_food")}
+			)
+	private Set<Food> food = new HashSet<>();
+
 	public Category() {
 		super();
 	}
-
-	public Category(Integer id, String name, Double price) {
+	
+	public Category(Integer id, String name, Double price, Set<Food> food) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.price = price;
+		this.food = food;
 	}
 	
 	public Integer getId() {
@@ -54,6 +68,57 @@ public class Category
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+	
+	public Set<Food> getFood() {
+		return food;
+	}
+
+	public void setFood(Set<Food> food) {
+		this.food = food;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((food == null) ? 0 : food.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (food == null) {
+			if (other.food != null)
+				return false;
+		} else if (!food.equals(other.food))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		return true;
 	}
 
 	@Override
