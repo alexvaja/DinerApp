42881fc2b877a60c0247bf877b2ea84652529d7e
WW930\A707350 <alexandru_vaja@yahoo.com>
2018-11-07 16:10:15 +0200

    Add in Food entity relation ManyToMany, HashCode and Equals

--- DinerApp/src/main/java/dinerapp/entity/Category.java
+++ DinerApp/src/main/java/dinerapp/entity/Category.java
@@ -32,7 +32,7 @@ public class Category
 			joinColumns={@JoinColumn(name="id_category")},
 			inverseJoinColumns={@JoinColumn(name="id_food")}
 			)
-	private Set<Food> food = new HashSet<>();
+	private Set<Food> foods = new HashSet<>();
 
 	public Category() {
 		super();
@@ -43,7 +43,7 @@ public class Category
 		this.id = id;
 		this.name = name;
 		this.price = price;
-		this.food = food;
+		this.foods = food;
 	}
 	
 	public Integer getId() {
@@ -71,18 +71,18 @@ public class Category
 	}
 	
 	public Set<Food> getFood() {
-		return food;
+		return foods;
 	}
 
 	public void setFood(Set<Food> food) {
-		this.food = food;
+		this.foods = food;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((food == null) ? 0 : food.hashCode());
+		result = prime * result + ((foods == null) ? 0 : foods.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((price == null) ? 0 : price.hashCode());
@@ -98,10 +98,10 @@ public class Category
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-		if (food == null) {
-			if (other.food != null)
+		if (foods == null) {
+			if (other.foods != null)
 				return false;
-		} else if (!food.equals(other.food))
+		} else if (!foods.equals(other.foods))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -123,6 +123,6 @@ public class Category
 
 	@Override
 	public String toString() {
-		return "Category [id=" + id + ", name=" + name + ", price=" + price + "]";
+		return "Category [id=" + id + ", name=" + name + ", price=" + price + ", foods=" + foods + "]";
 	}
 }
