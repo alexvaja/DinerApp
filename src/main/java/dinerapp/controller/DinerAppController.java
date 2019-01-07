da6c59a105fdd5aa56bcf306d816eda50acb27e6
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-01-07 16:07:49 +0200

    I solved all Warnings

--- src/main/java/dinerapp/controller/DinerAppController.java
+++ src/main/java/dinerapp/controller/DinerAppController.java
@@ -1,20 +1,18 @@
 package dinerapp.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
 
-import dinerapp.security.config.*;
+import dinerapp.security.config.WebUtils;
 
 @Controller
 @RequestMapping("/")
