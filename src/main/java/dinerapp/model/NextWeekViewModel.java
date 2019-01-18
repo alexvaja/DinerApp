54b2db733863832f0314df0eb0d79833662279d7
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-01-18 17:57:24 +0200

    Addes view- model class for delivered orders report

---
+++ src/main/java/dinerapp/model/NextWeekViewModel.java
@@ -0,0 +1,43 @@
+package dinerapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class NextWeekViewModel {
+
+//	private List<Order> orders;
+//	private List<Employee> employees;
+//	
+//	public NextWeekViewModel() {
+//		super();
+//		orders = new ArrayList<>();
+//		employees = new ArrayList<>();
+//		
+//	}
+//
+//	public List<Order> getOrders() {
+//		return orders;
+//	}
+//
+//	public void setOrders(List<Order> orders) {
+//		this.orders = orders;
+//	}
+//
+//	public List<Employee> getEmployees() {
+//		return employees;
+//	}
+//
+//	public void setEmployees(List<Employee> employees) {
+//		this.employees = employees;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "NextWeekViewModel []";
+//	}
+//	
+//	
+//	
+}
