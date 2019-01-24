bf81ac6b06f7b9c03ea46e9e10fe1d5cf388dfab
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-01-24 09:59:38 +0200

    Add new Repository class

---
+++ src/main/java/dinerapp/repository/OrderQuantityRepository.java
@@ -0,0 +1,9 @@
+package dinerapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dinerapp.model.entity.OrderQuantity;
+
+public interface OrderQuantityRepository extends CrudRepository<OrderQuantity, Integer> {
+
+}
