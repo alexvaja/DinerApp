e1b97070da3e5e589f1dc6646e664f53681d23f7
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-01-29 19:48:14 +0200

    Add Logger for this class

--- src/main/java/dinerapp/controller/UserReportController.java
+++ src/main/java/dinerapp/controller/UserReportController.java
@@ -4,11 +4,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,25 +25,21 @@ import dinerapp.model.UserReportViewModel;
 import dinerapp.model.dto.OrderDTO;
 import dinerapp.model.entity.Order;
 import dinerapp.model.entity.OrderQuantity;
-import dinerapp.model.entity.UserDiner;
 import dinerapp.repository.OrderQuantityRepository;
 import dinerapp.repository.OrderRepository;
-import dinerapp.repository.UserCantinaRepository;
 
 @Controller
-public class UserReportController 
-{
+public class UserReportController {
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	@Autowired
-	private UserCantinaRepository userDinerRepository;
-	
+
 	@Autowired
 	private OrderQuantityRepository orderQuantityRepository;
-	
+
 	private static final DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
 	@SessionScope
 	@GetMapping("/userReportView")
 	public String openNextWeekReportView(Model model, HttpSession session) {
@@ -53,24 +52,23 @@ public class UserReportController
 
 		return "views/userReportView";
 	}
-	
+
 	@PostMapping("/userReportView")
-	public String openNextWeekReportyyView(Model model, @SessionAttribute("userReportViewModel") UserReportViewModel userReportViewModel,
-														@RequestParam(value = "submit", required = false) String reqParam,
-														@RequestParam(value = "dropdown_list", required=false) String reportDate,
-														@RequestParam(value = "checkbox_list", required=false) String selectedUsers) {
+	public String openNextWeekReportyyView(Model model,
+			@SessionAttribute("userReportViewModel") UserReportViewModel userReportViewModel,
+			@RequestParam(value = "submit", required = false) String reqParam,
+			@RequestParam(value = "dropdown_list", required = false) String reportDate,
+			@RequestParam(value = "checkbox_list", required = false) String selectedUsers) {
 		System.out.println("Am intrat pe POST");
 		userReportViewModel.setDates(getAllNextDate());
 
 		List<Order> listOfOrdersFromTable = getAllOrderFromTable();
 		List<OrderQuantity> listOfOrdersQuantitiesFromTable = getAllOrderQuantityFromTable();
-		
-		
-		
+
 		List<Order> todayOrders = new ArrayList<>();
 		List<OrderQuantity> todayOrdersQuantities = new ArrayList<>();
-		
-		switch(reqParam) {
+
+		switch (reqParam) {
 		case "Search": {
 			System.out.println("Am intrat pe case-ul de Search!");
 
@@ -79,7 +77,7 @@ public class UserReportController
 					todayOrders.add(order);
 				}
 			}
-			
+
 			for (OrderQuantity orderQuantity : listOfOrdersQuantitiesFromTable) {
 				for (Order order : todayOrders) {
 					if (orderQuantity.getOrder().equals(order)) {
@@ -89,20 +87,21 @@ public class UserReportController
 			}
 
 			List<String> magicList = new ArrayList<>();
-			
+
 			for (Order order : todayOrders) {
 				String foodString = new String();
 				for (OrderQuantity orderQuantity : todayOrdersQuantities) {
 					if (order.equals(orderQuantity.getOrder())) {
-						foodString += (orderQuantity.getFoodd().getName() + " X" + orderQuantity.getQuantity().toString() + "  ");
+						foodString += (orderQuantity.getFoodd().getName() + " X"
+								+ orderQuantity.getQuantity().toString() + "  ");
 					}
 				}
 				magicList.add(foodString);
 			}
-			
+
 			List<OrderDTO> ordersDTO = new ArrayList<>();
 			for (int index = 0; index < todayOrders.size(); index++) {
-				OrderDTO orderDTO  = new OrderDTO();
+				OrderDTO orderDTO = new OrderDTO();
 				orderDTO.setOrder(todayOrders.get(index));
 				orderDTO.setToBeDeliveredFood(magicList.get(index));
 				ordersDTO.add(orderDTO);
@@ -113,83 +112,88 @@ public class UserReportController
 		}
 		case "Submit": {
 			System.out.println("Am intrat pe case-ul de Submit!");
-			
+
 			for (Order order : listOfOrdersFromTable) {
 				if (order.getDate().equals(reportDate)) {
 					todayOrders.add(order);
 				}
 			}
-				
-				String[] checkedOrderList = selectedUsers.split(",");
-				
-				for(int index = 0; index < checkedOrderList.length; index++) {
-					for (Order order : todayOrders) {
-						Order newOrder = order;
-						newOrder.setTaken(true);
-						
-						if (Integer.parseInt(checkedOrderList[index]) == order.getId()) {
-							orderRepository.save(newOrder);
-						}
+
+			String[] checkedOrderList = selectedUsers.split(",");
+
+			for (int index = 0; index < checkedOrderList.length; index++) {
+				for (Order order : todayOrders) {
+					Order newOrder = order;
+					newOrder.setTaken(true);
+
+					if (Integer.parseInt(checkedOrderList[index]) == order.getId()) {
+						orderRepository.save(newOrder);
 					}
 				}
-			
-			
+			}
+
 			break;
 		}
 		}
-		
+
 		return "views/userReportView";
 	}
-	
-	private List<Order> getAllOrderFromTable() { 
+
+	private List<Order> getAllOrderFromTable() {
 		Iterable<Order> list = orderRepository.findAll();
 		List<Order> searchedList = new ArrayList<>();
-		
+
 		for (Order order : list) {
 			searchedList.add(order);
 		}
-		
+
 		return searchedList;
 	}
-	
-	private List<UserDiner> getAllUserDinerFromTable() { 
-		Iterable<UserDiner> list = userDinerRepository.findAll();
-		List<UserDiner> searchedList = new ArrayList<>();
-		
-		for (UserDiner userDiner : list) {
-			searchedList.add(userDiner);
-		}
-		
-		return searchedList;
-	}	
-	
-	private List<OrderQuantity> getAllOrderQuantityFromTable() { 
+
+//	private List<UserDiner> getAllUserDinerFromTable() { 
+//		Iterable<UserDiner> list = userDinerRepository.findAll();
+//		List<UserDiner> searchedList = new ArrayList<>();
+//		
+//		for (UserDiner userDiner : list) {
+//			searchedList.add(userDiner);
+//		}
+//		
+//		return searchedList;
+//	}	
+
+	private List<OrderQuantity> getAllOrderQuantityFromTable() {
 		Iterable<OrderQuantity> list = orderQuantityRepository.findAll();
 		List<OrderQuantity> searchedList = new ArrayList<>();
-		
+
 		for (OrderQuantity orderQuantity : list) {
 			searchedList.add(orderQuantity);
 		}
-		
+
 		return searchedList;
 	}
-	
+
 	private List<String> getAllNextDate() {
 		List<String> searchedDate = new ArrayList<>();
+		Date date = new Date();
+		Calendar calendar = Calendar.getInstance();
+		System.out.println("Calendar: " + calendar);
+		System.out.println("DATA: " + date);
 		String currentDate = sdf.format(new Date());
 		
-		for(int dayIndex = 0 ;dayIndex <= 4 ;dayIndex++) {
+		
+
+		for (int dayIndex = 0; dayIndex <= 4; dayIndex++) {
 			searchedDate.add(incrementCurrentDayByIndex(currentDate, dayIndex));
 		}
-		
+
 		return searchedDate;
 	}
-	
+
 	private String incrementCurrentDayByIndex(String date, int index) {
 		return LocalDate.parse(date).plusDays(index).toString();
 	}
-	
-	private List<Order> getAllOrdersForGivenDate(String date) {
-		return null;
-	}
+
+//	private List<Order> getAllOrdersForGivenDate(String date) {
+//		return null;
+//	}
 }
