bf44a3bec5f3439688657c5d996011c3cd2657d5
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-02-27 11:23:34 +0200

    Finish error controller

--- src/main/java/dinerapp/controller/CustomErrorController.java
+++ src/main/java/dinerapp/controller/CustomErrorController.java
@@ -2,45 +2,57 @@ package dinerapp.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ErrorController {
- 
-    @RequestMapping(value = "errors", method = RequestMethod.GET)
-    public String renderErrorPage(Model model, HttpServletRequest httpRequest) {
-         
-        String errorMsg = "";
-        int httpErrorCode = getErrorCode(httpRequest);
- 
-        switch (httpErrorCode) {
-            case 400: {
-                errorMsg = "Http Error Code: 400. Bad Request";
-                break;
-            }
-            case 401: {
-                errorMsg = "Http Error Code: 401. Unauthorized";
-                break;
-            }
-            case 404: {
-                errorMsg = "Http Error Code: 404. Resource not found";
-                break;
-            }
-            case 500: {
-                errorMsg = "Http Error Code: 500. Internal Server Error";
-                break;
-            }
-        }
-        
-        model.addAttribute("errorMsg", errorMsg);
-        return "views/errorPage";
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "error";
     }
-     
-    private int getErrorCode(HttpServletRequest httpRequest) {
-        return (Integer) httpRequest
-          .getAttribute("javax.servlet.error.status_code");
-    }
+	
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+ 
+//    @RequestMapping(value = "errors", method = RequestMethod.GET)
+//    public String renderErrorPage(Model model, HttpServletRequest httpRequest) {
+//         
+//        String errorMsg = "";
+//        int httpErrorCode = getErrorCode(httpRequest);
+// 
+//        switch (httpErrorCode) {
+//            case 400: {
+//                errorMsg = "Http Error Code: 400. Bad Request";
+//                break;
+//            }
+//            case 401: {
+//                errorMsg = "Http Error Code: 401. Unauthorized";
+//                break;
+//            }
+//            case 404: {
+//                errorMsg = "Http Error Code: 404. Resource not found";
+//                break;
+//            }
+//            case 500: {
+//                errorMsg = "Http Error Code: 500. Internal Server Error";
+//                break;
+//            }
+//        }
+//        
+//        model.addAttribute("errorMsg", errorMsg);
+//        return "views/errorPage";
+//    }
+//     
+//    private int getErrorCode(HttpServletRequest httpRequest) {
+//        return (Integer) httpRequest
+//          .getAttribute("javax.servlet.error.status_code");
+//    }
 }
