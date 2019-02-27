1f5cf7a921d97512428e6fee0c46da546f0e2a1b
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-02-27 11:29:50 +0200

    Added prop

--- src/main/java/dinerapp/controller/CustomErrorController.java
+++ src/main/java/dinerapp/controller/CustomErrorController.java
@@ -1,12 +1,8 @@
 package dinerapp.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CustomErrorController implements ErrorController {
