a7b1ffebc3ff531f6ea71d8276642857e2d21d33
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-03-07 14:57:24 +0200

    Changed controller it to work with the new DTO

--- src/main/java/dinerapp/controller/StatisticsController.java
+++ src/main/java/dinerapp/controller/StatisticsController.java
@@ -16,88 +16,126 @@ import org.springframework.web.context.annotation.SessionScope;
 
 import dinerapp.exceptions.NewSessionException;
 import dinerapp.model.StatisticsViewModel;
+import dinerapp.model.dto.StatisticDTO;
 import dinerapp.model.entity.Order;
 import dinerapp.model.entity.UserDiner;
 import dinerapp.repository.OrderRepository;
 import dinerapp.repository.UserCantinaRepository;
 
 @Controller
-public class StatisticsController 
-{
+public class StatisticsController {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private OrderRepository orderRepo;
 	@Autowired
 	private UserCantinaRepository userDinerRepo;
-	
+
 	@ExceptionHandler({ NewSessionException.class })
-	public String sessionError() 
-	{	
+	public String sessionError() {
 		String mesaj = "incercare de acces nepermis";
 		LOGGER.error(mesaj);
 		return "views/loginView";
 	}
-	private List<Order> getListOfOrders() 
-	{
+
+	private List<Order> getListOfOrders() {
 		final Iterable<Order> list = orderRepo.findAll();
 		final List<Order> orderList = new ArrayList<>();
 		for (final Order order : list)
 			orderList.add(order);
 		return orderList;
 	}
-	private List<UserDiner> getListOfUsers()
-	{
+
+	private List<UserDiner> getListOfUsers() {
 		final Iterable<UserDiner> list = userDinerRepo.findAll();
 		final List<UserDiner> userList = new ArrayList<>();
-		for(final UserDiner user:list)
+		for (final UserDiner user : list)
 			userList.add(user);
 		return userList;
 	}
-	
+
 	@SessionScope
 	@GetMapping("/statisticsView")
-	public String getStatistics(Model model, HttpSession session) throws NewSessionException 
-	{
+	public String getStatistics(Model model, HttpSession session) throws NewSessionException {
 		LOGGER.info("Am intrat in getStatistics");
-		if (session.isNew()) 
-			throw new NewSessionException();			
+		if (session.isNew())
+			throw new NewSessionException();
 
-		StatisticsViewModel statisticsViewModel = new StatisticsViewModel();		
-		List<Order>orders = getListOfOrders();
-		List<UserDiner>users = getListOfUsers();	
-		List<Integer> placedOrders = new ArrayList<Integer>();
-		List<Integer> pickedUpOrders = new ArrayList<Integer>();
-		
-		for(int index = 0; index < users.size(); index++)
-		{
-			placedOrders.add(0);
-			pickedUpOrders.add(0);
+		StatisticsViewModel statisticsViewModel = new StatisticsViewModel();
+		List<Order> orders = getListOfOrders();
+		List<UserDiner> users = getListOfUsers();
+
+		List<Integer> placedOrders = new ArrayList<>(users.size());
+		List<Integer> pickedUpOrders = new ArrayList<>(users.size());
+
+//		for (int index = 0; index < users.size(); index++) {
+//			placedOrders.add(0);
+//			pickedUpOrders.add(0);
+//		}
+//		
+		for (int index = 0; index < users.size(); index++) {
+			System.out.println("PlacedOrders1 :" + placedOrders);
+			System.out.println("PickedUpOrder1" + pickedUpOrders);
 		}
 		
-		for(UserDiner user : users)
-		{
-			for(int i = 0; i < orders.size(); i++)
-			{
-				if(orders.get(i).getUserDiner().equals(user))
-				{
-					int value = placedOrders.get(i);
-					value++;
-					placedOrders.add(i, value);
-					if(orders.get(i).getTaken() == true)
-					{
-						int var = pickedUpOrders.get(i);
-						var++;
-						pickedUpOrders.add(i,var);
+
+		for (int i = 0; i < users.size(); i++) {
+			System.out.println("user " + i + ":" + users.get(i));
+			System.out.println("---------------------------------------");
+			int numberOfPlacedOrders = 0;
+			int numberOfPickedUpOrders = 0;
+			for (int index = 0; index < orders.size(); index++) {
+				System.out.println("order " + index + ":" + orders.get(index));
+				if (orders.get(index).getUserDiner().equals(users.get(i))) {
+					numberOfPlacedOrders++;
+//					int value = placedOrders.get(i);
+//					value++;
+//					placedOrders.add(i, value);
+					if (orders.get(index).getTaken() == true) {
+						numberOfPickedUpOrders++;
+//						int var = pickedUpOrders.get(i);
+//						var++;
+//						pickedUpOrders.add(i, var);
 					}
 				}
 			}
+			
+			placedOrders.add(new Integer(numberOfPlacedOrders));
+			pickedUpOrders.add(new Integer(numberOfPickedUpOrders));
+			
+			}
+		
+		List<String> x = new ArrayList<>();
+		
+		for (int index = 0; index < users.size(); index++) {
+			x.add(placedOrders.get(index) + "/" + pickedUpOrders.get(index));
+		}
+
+		System.out.println("PlacedOrders2 :" + placedOrders);
+		System.out.println("PickedUpOrder2" + pickedUpOrders);
+
+		List<StatisticDTO> statistics = new ArrayList<>();
+		
+		for (int index = 0; index < users.size(); index++) {
+			//StatisticDTO stat = new StatisticDTO(users.get(index), placedOrders.get(index), pickedUpOrders.get(index));
+			statistics.add(new StatisticDTO(users.get(index), placedOrders.get(index), pickedUpOrders.get(index)));
 		}
 		
-		statisticsViewModel.setPickedUpOrders(pickedUpOrders);
-		statisticsViewModel.setPlacedOrders(placedOrders);
-		statisticsViewModel.setUsers(users);
-		session.setAttribute("statisticsViewModel", statisticsViewModel);
+		statisticsViewModel.setStatistics(statistics);
 		
+//		statisticsViewModel.setPickedUpOrders(pickedUpOrders);
+//		statisticsViewModel.setPlacedOrders(placedOrders);
+//		statisticsViewModel.setUsers(users);
+//		statisticsViewModel.setX(x);
+		
+//		System.out.println("Aici afisez");
+//		
+//		System.out.println("usersss :" + users);
+//			
+//
+//		System.out.println();
+//		System.out.println(statisticsViewModel);
+		session.setAttribute("statisticsViewModel", statisticsViewModel);
+
 		return "views/statisticsView";
 	}
 
