d1bb33f663c7ac8230c09bfedbccce18400ec7e0
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-03-07 14:53:31 +0200

    Change all properties in this class to one list with DTO

--- src/main/java/dinerapp/model/StatisticsViewModel.java
+++ src/main/java/dinerapp/model/StatisticsViewModel.java
@@ -1,39 +1,35 @@
 package dinerapp.model;
-    
-import java.util.ArrayList;
-import java.util.List;   
-import dinerapp.model.entity.UserDiner;
-    
-public class StatisticsViewModel 
-{   
-	private List<UserDiner>users;
-	private List<Integer> placedOrders;
-	private List<Integer> pickedUpOrders;
-	
-	public StatisticsViewModel() 
-	{
+
+import java.util.List;
+
+import org.springframework.web.context.annotation.SessionScope;
+
+import dinerapp.model.dto.StatisticDTO;
+
+@SessionScope
+public class StatisticsViewModel {
+
+	private List<StatisticDTO> statistics;
+
+	public StatisticsViewModel() {
 		super();
-		this.users = new ArrayList<>();
-		this.placedOrders = new ArrayList<>();
-		this.pickedUpOrders = new ArrayList<>();
 	}
-	public List<UserDiner> getUsers() {
-		return users;
+
+	public StatisticsViewModel(List<StatisticDTO> statistics) {
+		super();
+		this.statistics = statistics;
 	}
-	public void setUsers(List<UserDiner> users) {
-		this.users = users;
+
+	public List<StatisticDTO> getStatistics() {
+		return statistics;
 	}
-	public List<Integer> getPlacedOrders() {
-		return placedOrders;
+
+	public void setStatistics(List<StatisticDTO> statistics) {
+		this.statistics = statistics;
 	}
-	public void setPlacedOrders(List<Integer> placedOrders) {
-		this.placedOrders = placedOrders;
+
+	@Override
+	public String toString() {
+		return "StatisticsViewModel [" + statistics + "]";
 	}
-	public List<Integer> getPickedUpOrders() {
-		return pickedUpOrders;
-	}
-	public void setPickedUpOrders(List<Integer> pickedUpOrders) {
-		this.pickedUpOrders = pickedUpOrders;
-	}
-	
 }
