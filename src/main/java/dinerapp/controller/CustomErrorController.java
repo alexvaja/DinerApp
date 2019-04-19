4cb74c56e89aa4032f28304eb4f86fed01610406
silviu_alex <sabau.silviu.bv@gmail.com>
2019-04-19 13:42:02 +0300

    Separated the errors in 404 and 500.

--- src/main/java/dinerapp/controller/CustomErrorController.java
+++ src/main/java/dinerapp/controller/CustomErrorController.java
@@ -1,58 +1,35 @@
 package dinerapp.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorController implements ErrorController {
+public class CustomErrorController implements ErrorController 
+{
 
-    @RequestMapping("/error")
-    public String handleError() {
-        //do something like logging
-        return "error";
-    }
+	@RequestMapping("/error")
+	public String renderErrorPage(Model model, HttpServletRequest httpRequest) 
+	{
+		Object status = httpRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if (status != null) 
+		{
+			Integer statusCode = Integer.valueOf(status.toString());
+			if(statusCode == HttpStatus.NOT_FOUND.value())
+				return "error/admin/A404Error";
+			if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+				return "error/admin/A500Error";
+		}
+		return "error/adminError";
+	}
 	
 	@Override
 	public String getErrorPath() {
-		return "/error";
+		return "/adminError";
 	}
-//	@Override
-//	public String getErrorPath() {
-//		return "/error";
-//	}
- 
-//    @RequestMapping(value = "errors", method = RequestMethod.GET)
-//    public String renderErrorPage(Model model, HttpServletRequest httpRequest) {
-//         
-//        String errorMsg = "";
-//        int httpErrorCode = getErrorCode(httpRequest);
-// 
-//        switch (httpErrorCode) {
-//            case 400: {
-//                errorMsg = "Http Error Code: 400. Bad Request";
-//                break;
-//            }
-//            case 401: {
-//                errorMsg = "Http Error Code: 401. Unauthorized";
-//                break;
-//            }
-//            case 404: {
-//                errorMsg = "Http Error Code: 404. Resource not found";
-//                break;
-//            }
-//            case 500: {
-//                errorMsg = "Http Error Code: 500. Internal Server Error";
-//                break;
-//            }
-//        }
-//        
-//        model.addAttribute("errorMsg", errorMsg);
-//        return "views/errorPage";
-//    }
-//     
-//    private int getErrorCode(HttpServletRequest httpRequest) {
-//        return (Integer) httpRequest
-//          .getAttribute("javax.servlet.error.status_code");
-//    }
 }
