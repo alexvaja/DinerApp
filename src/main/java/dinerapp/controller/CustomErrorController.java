8653314f88123e623da98391134875ef98838569
silviu_alex <sabau.silviu.bv@gmail.com>
2019-04-19 15:05:27 +0300

    Am terminat partea de mesaje de eroare separate in functie de user: admin sau employee. Nu am testat pe bucata lui flavius. Butonul de go home pe partea de employee te duce pe portal

--- src/main/java/dinerapp/controller/CustomErrorController.java
+++ src/main/java/dinerapp/controller/CustomErrorController.java
@@ -1,35 +1,65 @@
 package dinerapp.controller;
 
+import java.security.Principal;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import dinerapp.model.entity.Role;
+import dinerapp.model.entity.UserDiner;
+import dinerapp.repository.UserCantinaRepository;
+
 @Controller
-public class CustomErrorController implements ErrorController 
-{
+public class CustomErrorController implements ErrorController {
+
+	@Autowired
+	private UserCantinaRepository userRepo;
 
 	@RequestMapping("/error")
-	public String renderErrorPage(Model model, HttpServletRequest httpRequest) 
+	public String renderErrorPage(Model model, HttpServletRequest httpRequest, Principal principal) 
 	{
+		Role role = this.getUsersByRole(principal.getName());
 		Object status = httpRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		if (status != null) 
 		{
 			Integer statusCode = Integer.valueOf(status.toString());
-			if(statusCode == HttpStatus.NOT_FOUND.value())
-				return "error/admin/A404Error";
-			if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
-				return "error/admin/A500Error";
+			if (role.getName().equals("admin")) 
+			{
+				if (statusCode == HttpStatus.NOT_FOUND.value())
+					return "error/admin/A404Error";
+				if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+					return "error/admin/A500Error";
+			}
+			else if(role.getName().equals("employee"))
+			{
+				if (statusCode == HttpStatus.NOT_FOUND.value())
+					return "error/admin/E404Error";
+				if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+					return "error/admin/E500Error";
+			}
 		}
 		return "error/adminError";
 	}
-	
+
 	@Override
 	public String getErrorPath() {
 		return "/adminError";
+	}
+
+	private Role getUsersByRole(String name) {
+		Iterable<UserDiner> users = userRepo.findAll();
+		for (UserDiner user : users) {
+			if (user.getName().equals(name))
+				return user.getRoles().get(0);
+
+		}
+		return null;
 	}
 }
